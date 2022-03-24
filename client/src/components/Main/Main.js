3a9a3f29cca83900109314dbdd64d45fae0c7706
Christopher McGregor <christopher.mcgregor@hotmail.com>
2022-03-24 16:38:58 -0400

    Client code refactored to keep canvas from rerendering on brush and eraser size change, colour change and eraser functionality added

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,40 +1,148 @@
-import React from "react";
-import Canvas from "../Canvas/Canvas";
+import React, { useRef, useEffect, useState } from "react";
+import PaintTools from "../PaintTools/PaintTools";
 import Button from "../Button/Button";
 import "./Main.scss";
 
-export default class Main extends React.Component {
-  state = {
-    loadImage: false,
-    saveImage: false
+export default function Main() {
+  //Setting initial state and references for canvas
+  const canvasRef = useRef(null);
+  const coloursRef = useRef(null);
+  const ctxRef = useRef(null);
+  let [brushActive, setBrushActive] = useState(true);
+  let [eraserActive, setEraserActive] = useState(false);
+  let [isDrawing, setIsDrawing] = useState(false);
+  let [strokeStyle, setStrokeStyle] = useState("black");
+  let [lineWidth, setLineWidth] = useState(3);
+  let [eraserWidth, setEraserWidth] = useState(3);
+  let [clearCanvas, setClearCanvas] = useState(false);
+  let [loadImage, setLoadImage] = useState(false);
+  let [saveImage, setSaveImage] = useState(false);
+
+  //useEffect hook for canvas and tools
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    ctx.lineCap = "round";
+    ctx.lineJoin = "round";
+    ctx.strokeStyle = strokeStyle;
+    ctx.lineWidth = lineWidth;
+    ctx.imageSmoothingEnabled = false;
+    ctxRef.current = ctx;
+  }, [strokeStyle, lineWidth]);
+
+  //Get mouse location to keep mouse position in canvas on resize
+  let mx = 0;
+  let my = 0;
+  const getMouse = (event) => {
+    const canvas = canvasRef.current;
+    const rect = canvas.getBoundingClientRect();
+    let scaleX = canvas.width / rect.width;
+    let scaleY = canvas.height / rect.height;
+    mx = (event.clientX - rect.left) * scaleX;
+    my = (event.clientY - rect.top) * scaleY;
   };
 
-  render() {
-    const handleLoadImage = () => {
-      this.setState({ loadImage: true });
-    };
+  //Start drawing function
+  const startDraw = (event) => {
+    getMouse(event);
+    ctxRef.current.beginPath();
+    ctxRef.current.moveTo(mx, my);
+    setIsDrawing(true);
+  };
 
-    const handleSaveImage = () => {
-      this.setState({ saveImage: true });
-    };
+  //End drawing function
+  const endDraw = (event) => {
+    getMouse(event);
+    ctxRef.current.closePath();
+    setIsDrawing(false);
+  };
 
-    let { loadImage, saveImage } = this.state;
-    return (
-      <section className="homepage">
-        <Canvas loadImage={loadImage} saveImage={saveImage} />
-        <div>
-          <Button
-            onClick={handleLoadImage}
-            className="homepage__button--load"
-            text="Load Image"
-          />
-          <Button
-            onClick={handleSaveImage}
-            className="homepage__button--save"
-            text="Save Image"
-          />
-        </div>
-      </section>
-    );
+  //Drawing function
+  const draw = (event) => {
+    if (!isDrawing) {
+      return;
+    }
+    getMouse(event);
+    if (brushActive) {
+    ctxRef.current.lineTo(mx, my);
+    ctxRef.current.stroke();
+    }
+    if (eraserActive) {
+      ctxRef.current.clearRect(
+        mx - eraserWidth / 2,
+        my - eraserWidth / 2,
+        eraserWidth,
+        eraserWidth
+      );
+    }
+  };
+
+  const dot = (event) => {
+    getMouse(event);
+    if (brushActive) {
+    ctxRef.current.beginPath();
+    ctxRef.current.moveTo(mx, my);
+    ctxRef.current.lineTo(mx, my);
+    ctxRef.current.stroke();
+    }
+    if (eraserActive) {
+      ctxRef.current.clearRect(
+        mx - eraserWidth / 2,
+        my - eraserWidth / 2,
+        eraserWidth,
+        eraserWidth
+      );
+    }
+  };
+
+  //Loading and saving image handlers
+  const handleLoadImage = () => {
+    setLoadImage(true);
+  };
+
+  const handleSaveImage = () => {
+    setSaveImage(true);
+  };
+
+  if (clearCanvas) {
+    const canvas = canvasRef.current;
+    const ctx = ctxRef.current;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    setClearCanvas(false);
   }
+
+  return (
+    <section className="homepage">
+      <div>
+        <canvas
+          className="homepage__canvas"
+          ref={canvasRef}
+          onMouseDown={startDraw}
+          onMouseUp={endDraw}
+          onMouseMove={draw}
+          onClick={dot}
+        ></canvas>
+        <PaintTools
+          setBrushActive={setBrushActive}
+          setEraserActive={setEraserActive}
+          setLineWidth={setLineWidth}
+          setStrokeStyle={setStrokeStyle}
+          setEraserWidth={setEraserWidth}
+          setClearCanvas={setClearCanvas}
+        />
+      </div>
+      <div>
+        <Button
+          onClick={handleLoadImage}
+          className="homepage__button--load"
+          text="Load Image"
+        />
+        <Button
+          onClick={handleSaveImage}
+          className="homepage__button--save"
+          text="Save Image"
+        />
+      </div>
+    </section>
+  );
 }
---
+++ client/src/components/PaintTools/PaintTools.js
@@ -0,0 +1,59 @@
+import React from "react";
+import "./PaintTools.scss";
+
+export default function PaintTools(props) {
+  //Render component with all mouse events and references
+  return (
+    <div className="homepage__paint-tools">
+      <h2>Tools:</h2>
+      <h4
+        className="homepage__paint-tools--label"
+        onClick={() => {
+          props.setBrushActive(true);
+          props.setEraserActive(false);
+        }}
+      >
+        Brush Size
+      </h4>
+      <input
+        type="range"
+        min="1"
+        max="10"
+        onChange={(event) => {
+          props.setLineWidth(event.target.value);
+        }}
+      ></input>
+      <h4 className="homepage__paint-tools--label">Fill</h4>
+      <h4 className="homepage__paint-tools--label">Stamp</h4>
+      <h4 className="homepage__paint-tools--label"
+      onClick={() => {
+        props.setEraserActive(true);
+        props.setBrushActive(false);
+      }}>Eraser</h4>
+      <input
+        type="range"
+        min="1"
+        max="10"
+        onChange={(event) => {
+          props.setEraserWidth(event.target.value);
+        }}
+      ></input>
+      <h4 className="homepage__paint-tools--label">Colours</h4>
+      <input
+        className="homepage__paint-tools--color"
+        type="color"
+        onChange={(event) => {
+          props.setStrokeStyle(event.target.value);
+        }}
+      ></input>
+      <h4 className="homepage__paint-tools--label">Undo</h4>
+      <h4 className="homepage__paint-tools--label">Redo</h4>
+      <h4
+        className="homepage__paint-tools--label"
+        onClick={() => props.setClearCanvas(true)}
+      >
+        Clear All
+      </h4>
+    </div>
+  );
+}
