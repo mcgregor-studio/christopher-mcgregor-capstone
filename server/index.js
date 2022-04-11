e8406631a0fa144da620b78d94bac22bfba5bca7
Christopher McGregor <christopher.mcgregor@hotmail.com>
2022-04-10 20:18:18 -0400

    Replaced GitHub strategy with Google strategy, initial code for Passport use written, Galler.ai database created

--- server/index.js
+++ server/index.js
@@ -1,13 +1,12 @@
 const express = require("express");
-const expressSession = require('express-session');
-const helmet = require('helmet');
-const passport = require('passport');
-const GitHubStrategy = require('passport-github2').Strategy;
-const knex = require('knex')(require('./knexfile.js').development);
+const expressSession = require("express-session");
+const helmet = require("helmet");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth2").Strategy;
+const knex = require("knex")(require("./knexfile.js").development);
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-
 
 const port = process.env.PORT || 3100;
 const profileRoutes = require("./routes/profile");
@@ -25,14 +24,15 @@ app.use(
   expressSession({
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: true,
   })
 );
-app.use(cors({
+app.use(
+  cors({
     origin: true,
-    credentials: true
-  }
-));
+    credentials: true,
+  })
+);
 app.use("/", profileRoutes);
 
 app.listen(port, () => {
@@ -42,71 +42,42 @@ app.listen(port, () => {
 //Passport configuration
 app.use(passport.initialize());
 app.use(passport.session());
-passport.use(
-  new GitHubStrategy(
-    {
-      clientID: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: process.env.GITHUB_CALLBACK_URL
-    },
-    (_accessToken, _refreshToken, profile, done) => {
-      // For our implementation we don't need access or refresh tokens.
-      // Profile parameter will be the profile object we get back from GitHub
-      console.log('GitHub profile:', profile);
 
-      // First let's check if we already have this user in our DB
-      knex('users')
-        .select('id')
-        .where({ github_id: profile.id })
-        .then(user => {
-          if (user.length) {
-            // If user is found, pass the user object to serialize function
-            done(null, user[0]);
-          } else {
-            // If user isn't found, we create a record
-            knex('users')
-              .insert({
-                github_id: profile.id,
-                avatar_url: profile._json.avatar_url,
-                username: profile.username
-              })
-              .then(userId => {
-                // Pass the user object to serialize function
-                done(null, { id: userId[0] });
-              })
-              .catch(err => {
-                console.error('Error creating a user', err);
-              });
-          }
-        })
-        .catch(err => {
-          console.error('Error fetching a user', err);
-        });
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://yourdomain:3000/auth/google/callback",
+      passReqToCallback: true,
+    },
+    function (request, accessToken, refreshToken, profile, done) {
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return done(err, user);
+      });
     }
   )
 );
 
 passport.serializeUser((user, done) => {
-  console.log('serializeUser (user object):', user);
-
+  console.log("serializeUser (user object):", user);
   // Store only the user id in session
   done(null, user.id);
 });
 
 passport.deserializeUser((userId, done) => {
-  console.log('deserializeUser (user id):', userId);
-
+  console.log("deserializeUser (user id):", userId);
   // Query user information from the database for currently authenticated user
-  knex('users')
+  knex("users")
     .where({ id: userId })
-    .then(user => {
+    .then((user) => {
       // Remember that knex will return an array of records, so we need to get a single record from it
-      console.log('req.user:', user[0]);
+      console.log("req.user:", user[0]);
 
       // The full user object will be attached to request object as `req.user`
       done(null, user[0]);
     })
-    .catch(err => {
-      console.log('Error finding user', err);
+    .catch((err) => {
+      console.error("Error finding user", err);
     });
 });
