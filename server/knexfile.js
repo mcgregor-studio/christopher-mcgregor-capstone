6bc812dd0d6f0134f5583a47de5bd59ba8b387fd
Christopher McGregor <christopher.mcgregor@hotmail.com>
2022-05-08 16:59:08 -0400

    Uninstalled excess libraries

--- server/knexfile.js
+++ server/knexfile.js
@@ -4,9 +4,10 @@
  * @type { Object.<string, import("knex").Knex.Config> }
  */
  require("dotenv").config();
+ 
  module.exports = {
   production: {
-    client: "mysql",
-    connection: "mysql://yzos9zy25xrsr2lt:pdhory48lppys0ry@s465z7sj4pwhp7fn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/pt3ufuw8nhtarjjt",
+    client: process.env.DB_CLIENT,
+    connection: process.env.JAWSDB_URL,
   },
 };
