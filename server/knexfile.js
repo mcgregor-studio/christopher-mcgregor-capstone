b23cff51f22c4018d331958a50d55c7c9d1c1279
Christopher McGregor <christopher.mcgregor@hotmail.com>
2022-05-08 13:48:40 -0400

    Updated knexfile to use env configs

--- server/knexfile.js
+++ server/knexfile.js
@@ -3,10 +3,25 @@
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
+ require("dotenv").config();
 
- module.exports = {
+ const connections = {
+  development: {
+    client: process.env.DB_CLIENT,
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PW,
+      database: process.env.DB_NAME,
+    },
+  },
   production: {
-    client: "mysql",
+    client: process.env.DB_CLIENT,
     connection: process.env.JAWSDB_URL,
   },
 };
+
+module.exports = 
+  process.env.NODE_ENV === 'production'
+    ? connections.production
+    : connections.development;
