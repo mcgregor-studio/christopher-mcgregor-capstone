7e8a6519de0d02ec9fc808a1635b9ab06bd84b3b
Christopher McGregor <christopher.mcgregor@hotmail.com>
2022-05-08 14:11:43 -0400

    Updated knexfile for deployment

--- server/knexfile.js
+++ server/knexfile.js
@@ -4,24 +4,10 @@
  * @type { Object.<string, import("knex").Knex.Config> }
  */
  require("dotenv").config();
-
- const connections = {
-  development: {
-    client: process.env.DB_CLIENT,
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PW,
-      database: process.env.DB_NAME,
-    },
-  },
+ 
+ module.exports = {
   production: {
     client: process.env.DB_CLIENT,
     connection: process.env.JAWSDB_URL,
   },
 };
-
-module.exports = 
-  process.env.NODE_ENV === 'production'
-    ? connections.production
-    : connections.development;
