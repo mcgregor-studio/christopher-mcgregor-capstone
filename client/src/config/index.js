84cfe026fb346d3ce8a4c3280069713db881579d
Christopher McGregor <christopher.mcgregor@hotmail.com>
2022-05-08 11:47:17 -0400

    Changes to API call

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,1 +1,3 @@
-export const API_URL = "https://gallerai-server.herokuapp.com"
+export const API_URL = {
+    client: "https://gallerai-client.herokuapp.com",
+    server: "https://gallerai-server.herokuapp.com"}
