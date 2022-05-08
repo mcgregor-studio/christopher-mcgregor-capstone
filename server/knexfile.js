3f99539d590806c3cf84285e08014f52a5d302ea
Christopher McGregor <christopher.mcgregor@hotmail.com>
2022-05-07 23:57:53 -0400

    Update to formatting for clear all modal, package.json update

--- server/knexfile.js
+++ server/knexfile.js
@@ -3,15 +3,26 @@
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
- module.exports = {
-    development: {
-      client: 'mysql',
-      connection: {
-        host: '127.0.0.1',
-        user: 'root',
-        password: 'rootr00t',
-        database: 'galler.ai_users',
-        charset: 'utf8',
-      },
-    }
-  };
+
+const connections = {
+  development: {
+    client: 'mysql',
+    connection: {
+      host: '127.0.0.1',
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PW,
+      database: 'galler.ai_users',
+      charset: 'utf8',
+    },
+  },
+  production: {
+    client: 'mysql',
+    connection: process.env.JAWSDB_URL
+  }
+
+}
+ module.exports = 
+    process.env.NODE_ENV === 'production'
+    ? connections.production
+    : connections.development;
+  ;
