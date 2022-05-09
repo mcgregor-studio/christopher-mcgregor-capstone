a3f46d4aceb86d05ca3ed63973a61ad4703a77e9
Christopher McGregor <christopher.mcgregor@hotmail.com>
2022-05-08 21:44:25 -0400

    Update to auth to check for axios calls

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -45,6 +45,7 @@ router.get(
 
 //User profile GET request
 router.get("/profile", (req, res) => {
+  console.log(req.user)
   if (req.user === undefined) {
     return res.status(401).json({ message: "Unauthorized" });
   }
