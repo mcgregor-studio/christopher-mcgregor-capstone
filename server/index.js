14e2b07c78ac98c6ed0ce15fa6b928285a7e0c1e
Christopher McGregor <christopher.mcgregor@hotmail.com>
2022-05-09 23:56:56 -0400

    Added returns

--- server/index.js
+++ server/index.js
@@ -68,7 +68,7 @@ passport.use(
         .where({ g_id: profile.id })
         .then((user) => {
           if (user.length) {
-            done(null, user[0]);
+            return done(null, user[0]);
           } else {
             knex("users")
               .insert({
@@ -76,7 +76,7 @@ passport.use(
                 username: profile.name.givenName,
               })
               .then((userId) => {
-                done(null, { g_id: userId[0] });
+                return done(null, { g_id: userId[0] });
               })
               .catch((e) => console.error("Error creating a user:", e));
           }
@@ -88,7 +88,7 @@ passport.use(
 
 passport.serializeUser((user, done) => {
   console.log("serializeUser (user object):", user);
-  done(null, user.g_id);
+  return done(null, user.g_id);
 });
 
 passport.deserializeUser((userId, done) => {
@@ -97,7 +97,7 @@ passport.deserializeUser((userId, done) => {
     .where({ g_id: userId })
     .then((user) => {
       console.log("req.user:", user[0]);
-      done(null, user[0]);
+      return done(null, user[0]);
     })
     .catch((err) => {
       console.error("Error finding user", err);
